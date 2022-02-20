26274e086fdecbcbbb7160be0665e0fc6cba04f5
cjdomacena <cjdomacena@yahoo.com>
2022-02-20 13:41:39 -0500

    Created carousel and navigation

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+
+
+function carousel(img_path)
+{
+	const carousel_container = document.getElementById('carousel-container')
+	carousel_container.style.backgroundImage = `url("${ img_path }")`
+}
+
+function carousel_change()
+{
+	const img_paths =
+		['/../src/images/product-1.png',
+			'../src/images/product-2.png',
+			'../src/images/product-3.png'
+		]
+	let i = 1
+	// Default bg img
+	document.getElementById('carousel-container').style.backgroundImage = `url("${img_paths[0]}")`
+	setInterval(() =>
+	{
+		carousel(img_paths[i])
+		i = i < 2 ? i + 1 : 0 // if i less than 2, increment i, if goes greater than 2 reset back to zero
+ 	}, 3000)
+
+}
+
+carousel_change()
